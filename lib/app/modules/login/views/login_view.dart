3325ae3f1e3937f4fca91f020023c9a8904bab43
SuryaRf <ayruzrahmad@gmail.com>
2025-01-06 23:18:03 +0700

    integrate login to firebase

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:myskadika/app/modules/navigation_bar/views/navigation_bar_view.dart';
 import '../controllers/login_controller.dart';
-
-const users = {
-  'agus': '12345',
-  'hunter@gmail.com': 'hunter',
-};
 
 class LoginView extends GetView<LoginController> {
   const LoginView({super.key});
   Duration get loginTime => const Duration(milliseconds: 2250);
 
-  Future<String?> _authUser(LoginData data) {
-    debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
+  Future<String?> _authUser(LoginData data) async {
+    try {
+      // Ambil data pengguna dari Firestore
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('account')
+          .where('username', isEqualTo: data.name)
+          .where('password', isEqualTo: data.password)
+          .get();
+
+      if (querySnapshot.docs.isEmpty) {
+        return 'Username atau password salah.';
       }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
+      return null; // Login berhasil
+    } catch (e) {
+      return 'Terjadi kesalahan: $e';
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'My Skadika',
-    
       logo: const AssetImage('lib/assets/logo.png'),
       onLogin: _authUser,
       onSignup: (_) => Future(() => null),
@@ -54,9 +54,9 @@ class LoginView extends GetView<LoginController> {
       ),
       userValidator: (value) {
         if (value == null || value.isEmpty) {
-          return 'Username cannot be empty';
+          return 'Username tidak boleh kosong';
         }
-        return null; // Tidak ada validasi format email
+        return null;
       },
       theme: LoginTheme(
         primaryColor: Colors.green,
@@ -74,8 +74,6 @@ class LoginView extends GetView<LoginController> {
           elevation: 5.0,
         ),
       ),
-    
-    
       hideForgotPasswordButton: true, // Menyembunyikan tombol Forgot Password
     );
   }
