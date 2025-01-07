795db7e5ff92f3318d295f64ff635527d39675d7
SuryaRf <ayruzrahmad@gmail.com>
2025-01-07 16:12:20 +0700

    fix ui add mapel

--- lib/app/modules/detail_mapel/views/add_new_mapel.dart
+++ lib/app/modules/detail_mapel/views/add_new_mapel.dart
@@ -57,6 +57,7 @@ class _AddNewMapelState extends State<AddNewMapel> {
         _kodeController.clear();
         _namaGuruController.clear();
         _isiController.clear();
+        _jamController.clear();
       } catch (e) {
         Get.snackbar(
           'Error',
@@ -75,61 +76,133 @@ class _AddNewMapelState extends State<AddNewMapel> {
         title: const Text('Tambah Mata Pelajaran'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(labelText: 'Nama Mapel'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Nama Mapel tidak boleh kosong';
-                  }
-                  return null;
-                },
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: const Text(
+                        'Form Tambah Mapel',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    _buildTextField(
+                      controller: _nameController,
+                      label: 'Nama Mapel',
+                      hintText: 'Masukkan nama mapel',
+                      icon: Icons.book,
+                      validator: (value) => value == null || value.isEmpty
+                          ? 'Nama Mapel tidak boleh kosong'
+                          : null,
+                    ),
+                    const SizedBox(height: 15),
+                    _buildTextField(
+                      controller: _descriptionController,
+                      label: 'Deskripsi',
+                      hintText: 'Masukkan deskripsi mapel',
+                      icon: Icons.description,
+                    ),
+                    const SizedBox(height: 15),
+                    _buildTextField(
+                      controller: _jadwalController,
+                      label: 'Hari',
+                      hintText: 'Masukkan hari jadwal',
+                      icon: Icons.calendar_today,
+                    ),
+                    const SizedBox(height: 15),
+                    _buildTextField(
+                      controller: _jamController,
+                      label: 'Jam',
+                      hintText: 'Masukkan jam jadwal',
+                      icon: Icons.access_time,
+                    ),
+                    const SizedBox(height: 15),
+                    _buildTextField(
+                      controller: _kodeController,
+                      label: 'Kode Mapel',
+                      hintText: 'Masukkan kode mapel',
+                      icon: Icons.code,
+                    ),
+                    const SizedBox(height: 15),
+                    _buildTextField(
+                      controller: _namaGuruController,
+                      label: 'Nama Guru',
+                      hintText: 'Masukkan nama guru',
+                      icon: Icons.person,
+                    ),
+                    const SizedBox(height: 15),
+                    _buildTextField(
+                      controller: _isiController,
+                      label: 'Isi Mapel',
+                      hintText: 'Masukkan isi mapel',
+                      icon: Icons.edit,
+                      validator: (value) => value == null || value.isEmpty
+                          ? 'Isi Mapel tidak boleh kosong'
+                          : null,
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: ElevatedButton(
+                        
+                        onPressed: _addMapel,
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 50,
+                            vertical: 15,
+                            
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: const Text(
+                          'Simpan Mapel',
+                          style: TextStyle(fontSize: 16, color: Colors.green),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              TextFormField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(labelText: 'Deskripsi'),
-              ),
-              TextFormField(
-                controller: _jadwalController,
-                decoration: const InputDecoration(labelText: 'Hari'),
-              ),
-              TextFormField(
-                controller: _jamController,
-                decoration: const InputDecoration(labelText: 'Jam'),
-              ),
-              TextFormField(
-                controller: _kodeController,
-                decoration: const InputDecoration(labelText: 'Kode Mapel'),
-              ),
-              TextFormField(
-                controller: _namaGuruController,
-                decoration: const InputDecoration(labelText: 'Nama Guru'),
-              ),
-              TextFormField(
-                controller: _isiController,
-                decoration: const InputDecoration(labelText: 'Isi Mapel'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Isi Mapel tidak boleh kosong';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _addMapel,
-                child: const Text('Simpan Mapel'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    String? hintText,
+    IconData? icon,
+    String? Function(String?)? validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hintText,
+        prefixIcon: Icon(icon, color: Colors.green,),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      validator: validator,
     );
   }
 }
