89d01bbb00691e180c5ce18b18ce1ae62fdd6c65
SuryaRf <ayruzrahmad@gmail.com>
2025-01-12 00:08:17 +0700

    controller teacher exam

--- lib/app/modules/teacher_manage_exams/controllers/teacher_manage_exams_controller.dart
+++ lib/app/modules/teacher_manage_exams/controllers/teacher_manage_exams_controller.dart
@@ -1,23 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class TeacherManageExamsController extends GetxController {
-  //TODO: Implement TeacherManageExamsController
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  final title = ''.obs;
+  final questions = <Map<String, dynamic>>[].obs;
+
+  void addQuestion(String questionText, List<String> options, String correctAnswer) {
+    final questionId = DateTime.now().millisecondsSinceEpoch.toString();
+    questions.add({
+      "questionId": questionId,
+      "questionText": questionText,
+      "options": options,
+      "correctAnswer": correctAnswer,
+    });
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<void> saveExam() async {
+    final examId = DateTime.now().millisecondsSinceEpoch.toString();
+    try {
+      await firestore.collection('exams').doc(examId).set({
+        "examId": examId,
+        "title": title.value,
+        "questions": questions,
+      });
+      Get.snackbar("Success", "Exam has been created successfully");
+    } catch (e) {
+      Get.snackbar("Error", "Failed to create exam: $e");
+    }
   }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
